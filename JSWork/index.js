d6724cc00d5c9ff6758c6f8bada94254149ae418
Manik023sangal <maniksangal46@gmail.com>
2024-10-18 08:56:23 +0530

    Js Introduction

---
+++ JSWork/index.js
@@ -0,0 +1,16 @@
+console.log("Hello JS");
+
+// var a = 12;
+// if(a > 10){
+//     let b = 23;
+//     a = 40;
+//     console.log("b = " + b);
+// }
+// console.log("Hi, a = "+a);
+
+function fun(a,b){
+    console.log("hello");
+    return a + b;
+}
+let data = fun(23,45);
+console.log(data);
